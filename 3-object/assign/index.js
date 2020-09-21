4d9615eaabacad366f51c85c9e57da34e3fb8686
Summerdise <350464517@qq.com>
2020-09-21 22:06:36 +0800

    [zmc] feat: complete assign test

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,11 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  var copy = { serialNumber: '12345' };
+  var copy2 = {
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  return Object.assign(copy, source, copy2);
 }
