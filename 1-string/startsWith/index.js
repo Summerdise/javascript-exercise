31c06208e7845c53dc38ad4c3d4d02ed2d8f922a
Summerdise <350464517@qq.com>
2020-09-21 15:58:46 +0800

    [zmc] feat: complete string test

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,5 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const afterFilter = collection.filter(str => str.startsWith('粤'));
+  return afterFilter.length;
 }
